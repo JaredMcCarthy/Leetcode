a12e2a98f079bc704d4a51ffb29e1f2aaffe7cdb
jared <jared.lopezmccarthy05@gmail.com>
2026-01-04 03:13:13 -0600

    Actualizar solución del problema 3542 de Leetcode

--- Soluciones/3542. Minimum Operations to Convert All Elements to Zero/Leetcode_3542.py
+++ Soluciones/3542. Minimum Operations to Convert All Elements to Zero/Leetcode_3542.py
@@ -1,43 +1,41 @@
 
 class Solution:
-    def minOperations(self, nums):
-    	contador = 0
+	def minOperations(self, nums):
+		contador = 0
 
-    	while any( x != 0 for x in nums):
-    		grupos = self.separar_no_ceros(nums)
+		while any(x != 0 for x in nums):
+			grupos = self.separar_no_ceros(nums)
 
-    		for grupo in grupos:
-    			minimo_grupo= min(nums[idx] for idx in grupo)
+			for grupo in grupos:
+				minimo_grupo = min(nums[idx] for idx in grupo)
 
-    			for posicion_original in grupo:
-    				nums[posicion_original] = nums[posicion_original]  - minimo_grupo
-    			contador = contador + 1
+				for posicion_original in grupo:
+					nums[posicion_original] = nums[posicion_original] - minimo_grupo
+				contador = contador + 1
 
-    	return contador
+		return contador
 
-    def separar_no_ceros(self, nums):
-    	grupos = []
-    	grupo_actual = []
+	def separar_no_ceros(self, nums):
+		grupos = []
+		grupo_actual = []
 
-    	for indice in range(len(nums)):
-    		if nums[indice] != 0:
-    			grupo_actual.append(indice)
-    		else:
-    			if grupo_actual:
-    				grupos.append(grupo_actual)
-    				grupo_actual = []
+		for indice in range(len(nums)):
+			if nums[indice] != 0:
+				grupo_actual.append(indice)
+			else:
+				if grupo_actual:
+					grupos.append(grupo_actual)
+					grupo_actual = []
 
-    	if grupo_actual:
-    		grupos.append(grupo_actual)
+		if grupo_actual:
+			grupos.append(grupo_actual)
 
-    	return grupos
-
-
+		return grupos
 
 sol = Solution()
-print(sol.minOperations([0, 2]))  #1
-print(sol.minOperations([3, 1, 2, 1])) #3  
-print(sol.minOperations([1, 2, 1, 2, 1, 2])) #4  
+print(sol.minOperations([0, 2]))  # 1
+print(sol.minOperations([3, 1, 2, 1]))  # 3
+print(sol.minOperations([1, 2, 1, 2, 1, 2]))  # 4
 
 
 
