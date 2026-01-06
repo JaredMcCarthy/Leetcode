1a36a9420365c220408785a48e8d0acfa2e5690e
jared <jared.lopezmccarthy05@gmail.com>
2026-01-05 22:34:57 -0600

    Agregar solución Leetcode 5: Longest Palindromic Substring

---
+++ Soluciones/5. Longest Palindromic Substring/Leetcode_5.py
@@ -0,0 +1,24 @@
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        length = len(s) * 2
+        index_longest = offset_longest = 0
+        for index in range(length):
+            offset = 0
+            for offset in range(1 + index % 2, min(index, length - index), 2):
+                if s[(index - offset) // 2] != s[(index + offset) // 2]:
+                    offset -= 2
+                    break
+
+            if offset > offset_longest:
+                index_longest = index
+                offset_longest = offset
+        return s[(index_longest - offset_longest) // 2: (index_longest + offset_longest) // 2 + 1] 
+
+
+if __name__ == "__main__":
+	sol = Solution()
+	print(sol.longestPalindrome("cbbd"))
+	print(sol.longestPalindrome("racecar"))
+	print(sol.longestPalindrome("abc"))
