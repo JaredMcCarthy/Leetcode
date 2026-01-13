b1b229661ca38f7d0e1474e662276e39db803df0
jared <jared.lopezmccarthy05@gmail.com>
2026-01-12 18:13:49 -0600

    actualizadno ejercicio 12 de leetcode desde ya dias

---
+++ Soluciones/12. Integer to Roman/Leetcode_12.py
@@ -0,0 +1,13 @@
+
+class Solution(object):
+    def intToRoman(self, num):
+        numeros_romanos = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+
+        resultado = []
+
+        for valor, simbolo in numeros_romanos:
+            while num >= valor:
+                resultado.append(simbolo)
+                num -= valor
+
+        return "".join(resultado)
