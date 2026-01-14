baae6f435fcc211e413e82a1b28daa895504ab40
jared <jared.lopezmccarthy05@gmail.com>
2026-01-13 20:19:38 -0600

    agregando nuevo problema de leetcode resuelto

---
+++ Soluciones/14. Longest Common Prefix/Leetcode_14.py
@@ -0,0 +1,19 @@
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for i in range(1, len(strs)):
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
+
+sol = Solution()
+print(sol.longestCommonPrefix(["flower","flow","flight"]))
+print(sol.longestCommonPrefix(["dog","racecar","car"]))
